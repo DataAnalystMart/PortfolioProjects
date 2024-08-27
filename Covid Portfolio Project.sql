1af0255353fca34f3619e24eccccd13b04819fc5
Martin <157241100+DataAnalystMart@users.noreply.github.com>
2024-08-27 19:39:31 +0800

    Covid Portfolio Project.sql

--- Covid Portfolio Project.sql
+++ Covid Portfolio Project.sql
@@ -1,74 +1,73 @@
-select *
-from PortfolioProject..CovidDeaths
-where continent is not null
-order by 3,4
+Select *
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Order by 3,4
 
 
-select location,date,total_cases,new_cases,total_deaths,population
-from PortfolioProject..CovidDeaths
-order by 1,2
+Select location,date,total_cases,new_cases,total_deaths,population
+From PortfolioProject..CovidDeaths
+Order by 1,2
 
 
 -- Total Cases vs Total Deaths
 
-select location,date,total_cases,total_deaths,population,(total_deaths/total_cases)*100 as DeathPercentage
-from PortfolioProject..CovidDeaths
+Select location,date,total_cases,total_deaths,population,(total_deaths/total_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
 Where location like 'Philippines'
-order by 1,2
+Order by 1,2
 
 -- Percentage of population got covid
-select location,date,total_cases,population,(total_cases/population)*100 as DeathPercentage
-from PortfolioProject..CovidDeaths
+Select location,date,total_cases,population,(total_cases/population)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
 Where location like 'Philippines'
-order by 1,2
-
+Order by 1,2
 
 
 
 --- Countries with highers infection rate comapared to Population
 
-select location,population,MAX(total_cases)as HighestInfectionCount,population,Max((total_cases/population))*100 as InfectedPercentPopulation
-from PortfolioProject..CovidDeaths
+Select location,population,MAX(total_cases)as HighestInfectionCount,population,Max((total_cases/population))*100 as InfectedPercentPopulation
+From PortfolioProject..CovidDeaths
 --Where location like 'Philippines'
-group by location,population
-order by InfectedPercentPopulation desc
+Group by location,population
+Order by InfectedPercentPopulation desc
 
 
 --- Showing Contries with the Highest Death Count per Population
 
-select location,MAX(cast (total_deaths as int))as TotalDeathCount 
-from PortfolioProject..CovidDeaths
+Select location,MAX(cast (total_deaths as int))as TotalDeathCount 
+From PortfolioProject..CovidDeaths
 --Where location like 'Philippines'
-where continent is null
-group by location
-order by TotalDeathCount desc
+Where continent is null
+Group by location
+Order by TotalDeathCount desc
 
 --By continent
 
 -- Showing the continents with highest death count per population
 
-select continent,MAX(cast (total_deaths as int))as TotalDeathCount
-from PortfolioProject..CovidDeaths
+Select continent,MAX(cast (total_deaths as int))as TotalDeathCount
+From PortfolioProject..CovidDeaths
 	
 --Where location like ex: 'Philippines'
-where continent is not null
-group by continent
-order by TotalDeathCount desc
+Where continent is not null
+Group by continent
+Order by TotalDeathCount desc
 
 
 -- Global Numbers
 
-select date,SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
-from PortfolioProject..CovidDeaths
+Select date,SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
 Where continent is not null
 Group by date
-order by 1,2
+Order by 1,2
 
 -- world total deaths and percentage
 
-select SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
-from PortfolioProject..CovidDeaths
-order by 1,2
+Select SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Order by 1,2
 
 
 -- Total population vs vaccinated 
@@ -81,7 +80,7 @@ Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 	Where dea.continent is not null
-	order by 2,3
+	Order by 2,3
 
 --Using CTE
 
